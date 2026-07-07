260cd4575d966e3db4f0316ee8aafa5bbdc17f72
varinda <varindaptrn@gmail.com>
2026-07-07 12:30:09 +0530

    feat: add user auth with register and login API

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
 
@@ -11,6 +12,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.send('CodePulse Server is running!');
