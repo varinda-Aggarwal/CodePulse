6ac7365111868b911aef0ba22d8d2db23a365011
varinda <varindaptrn@gmail.com>
2026-07-20 15:14:44 +0530

    Remove manual achieved-update route, keep automatic goal tracking

--- server/routes/goalRoutes.js
+++ server/routes/goalRoutes.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getGoal, setGoal, updateAchieved } = require('../controllers/goalController');
+const { getGoal, setGoal } = require('../controllers/goalController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.route('/').get(protect, getGoal).post(protect, setGoal);
-router.route('/achieved').put(protect, updateAchieved);
 
 module.exports = router;
