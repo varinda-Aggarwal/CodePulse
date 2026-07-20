134fc1239c67a6195ce26cdf4023b56cc04441bf
varinda <varindaptrn@gmail.com>
2026-07-20 15:14:30 +0530

    Make goal achieved count automatic based on problems solved today

--- server/controllers/goalController.js
+++ server/controllers/goalController.js
@@ -1,13 +1,35 @@
 const Goal = require('../models/Goal');
+const Problem = require('../models/Problem');
+
+// Helper: count problems solved today
+const getTodayAchievedCount = async (userId) => {
+    const startOfDay = new Date();
+    startOfDay.setHours(0, 0, 0, 0);
+    const endOfDay = new Date();
+    endOfDay.setHours(23, 59, 59, 999);
+
+    return await Problem.countDocuments({
+        user: userId,
+        dateSolved: { $gte: startOfDay, $lte: endOfDay }
+    });
+};
 
 // Get today's goal
 const getGoal = async (req, res) => {
     try {
         const today = new Date().toISOString().split('T')[0];
-        const goal = await Goal.findOne({ user: req.user._id, date: today });
+        const achieved = await getTodayAchievedCount(req.user._id);
+
+        let goal = await Goal.findOne({ user: req.user._id, date: today });
 
         if (!goal) {
-            return res.status(200).json({ target: 0, achieved: 0, date: today });
+            return res.status(200).json({ target: 0, achieved, date: today });
+        }
+
+        // Keep stored achieved in sync with actual problems solved today
+        if (goal.achieved !== achieved) {
+            goal.achieved = achieved;
+            await goal.save();
         }
 
         res.status(200).json(goal);
@@ -21,16 +43,19 @@ const setGoal = async (req, res) => {
     try {
         const { target } = req.body;
         const today = new Date().toISOString().split('T')[0];
+        const achieved = await getTodayAchievedCount(req.user._id);
 
         let goal = await Goal.findOne({ user: req.user._id, date: today });
 
         if (goal) {
             goal.target = target;
+            goal.achieved = achieved;
             await goal.save();
         } else {
             goal = await Goal.create({
                 user: req.user._id,
                 target,
+                achieved,
                 date: today
             });
         }
@@ -41,23 +66,4 @@ const setGoal = async (req, res) => {
     }
 };
 
-// Update achieved count
-const updateAchieved = async (req, res) => {
-    try {
-        const today = new Date().toISOString().split('T')[0];
-        const goal = await Goal.findOne({ user: req.user._id, date: today });
-
-        if (!goal) {
-            return res.status(404).json({ message: 'No goal set for today' });
-        }
-
-        goal.achieved = req.body.achieved;
-        await goal.save();
-
-        res.status(200).json(goal);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
-
-module.exports = { getGoal, setGoal, updateAchieved };
+module.exports = { getGoal, setGoal };
