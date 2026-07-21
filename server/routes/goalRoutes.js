bfdc52b5ee6d83e5fce056052ee3eb95768213a1
varinda <varindaptrn@gmail.com>
2026-07-21 08:36:26 +0530

    Add route for fetching monthly goal history

--- server/routes/goalRoutes.js
+++ server/routes/goalRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getGoal, setGoal } = require('../controllers/goalController');
+const { getGoal, setGoal, getGoalHistory } = require('../controllers/goalController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.route('/').get(protect, getGoal).post(protect, setGoal);
+router.get('/history', protect, getGoalHistory);
 
 module.exports = router;
