5fff515c3ec99f88daa01a42418f4389800c126a
varinda <varindaptrn@gmail.com>
2026-07-29 15:02:21 +0530

    refactor(layout): restructure authenticated layout to use Sidebar + TopBar instead of full-width Navbar

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,16 +1,14 @@
-import Navbar from './Navbar';
 import Sidebar from './Sidebar';
+import TopBar from './TopBar';
 
 const Layout = ({ children }) => {
     return (
-        <div className="min-h-screen bg-gray-50 dark:bg-slate-950 transition-colors">
-            <Navbar />
-            <div className="flex">
-                <Sidebar />
-                <main className="flex-1 p-8">
-                    {children}
-                </main>
-            </div>
+        <div className="min-h-screen bg-surface-bg transition-colors flex">
+            <Sidebar />
+            <main className="flex-1 p-8">
+                <TopBar />
+                {children}
+            </main>
         </div>
     );
 };
