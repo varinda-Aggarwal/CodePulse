931d16d9c07400403f7cf0ca4a764c507c302db5
varinda <varindaptrn@gmail.com>
2026-07-29 15:07:57 +0530

    Return firstName, lastName, and photo in login and register response

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -156,6 +156,9 @@ const loginUser = async (req, res) => {
             _id: user._id,
             username: user.username,
             email: user.email,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            photo: user.photo,
             token
         });
 
