ac1d4d074927f062992aeb6cfcd29b7e404b42ad
varinda <varindaptrn@gmail.com>
2026-08-05 23:25:18 +0530

    Read auth token from sessionStorage as well as localStorage for Remember Me support

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -5,7 +5,7 @@ const API = axios.create({
 });
 
 API.interceptors.request.use((req) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem('token') || sessionStorage.getItem('token');
     if (token) {
         req.headers.Authorization = `Bearer ${token}`;
     }
