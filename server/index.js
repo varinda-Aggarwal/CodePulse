6274129a17293e5ee16632a4402b1301ba8d3190
varinda <varindaptrn@gmail.com>
2026-08-23 22:51:28 +0530

    Force IPv4 DNS resolution to fix ENETUNREACH errors connecting to Gmail SMTP

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
