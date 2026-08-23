c995873352ebaabe44ecd115aba9947bb44f370b
varinda <varindaptrn@gmail.com>
2026-08-23 22:59:56 +0530

    Use explicit SMTP config with longer timeout for Gmail connections

--- server/utils/sendOtpEmail.js
+++ server/utils/sendOtpEmail.js
@@ -1,13 +1,15 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_APP_PASSWORD
-    }
+    },
+    connectionTimeout: 20000
 });
-
 const sendOtpEmail = async (email, otp) => {
     await transporter.sendMail({
         from: `"CodePulse" <${process.env.EMAIL_USER}>`,
--- server/utils/sendResetEmail.js
+++ server/utils/sendResetEmail.js
@@ -1,11 +1,14 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_APP_PASSWORD
-    }
+    },
+    connectionTimeout: 20000
 });
 
 const sendResetEmail = async (email, resetLink) => {
