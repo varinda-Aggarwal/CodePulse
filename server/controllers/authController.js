3cf5a35ce7b695db533150b3706aaec547325e4b
varinda <varindaptrn@gmail.com>
2026-08-25 10:27:06 +0530

    Use FRONTEND_URL env variable for password reset link instead of hardcoded localhost

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -147,7 +147,7 @@ const forgotPassword = async (req, res) => {
         const token = crypto.randomBytes(32).toString('hex');
         await PasswordReset.create({ email, token });
 
-        const resetLink = `http://localhost:3000/reset-password/${token}`;
+        const resetLink = `${process.env.FRONTEND_URL}/reset-password/${token}`;
         await sendResetEmail(email, resetLink);
 
         res.status(200).json({ message: 'If that email exists, a reset link has been sent' });
